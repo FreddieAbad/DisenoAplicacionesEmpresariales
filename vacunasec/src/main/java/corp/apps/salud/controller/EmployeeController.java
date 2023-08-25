1e26f09dad39fb7e3d0acbe2831578e0becfeff1
freddy <freddy.abadl@ucuenca.edu.ec>
2023-08-24 19:14:37 -0500

    Add verb Delete

--- vacunasec/src/main/java/corp/apps/salud/controller/EmployeeController.java
+++ vacunasec/src/main/java/corp/apps/salud/controller/EmployeeController.java
@@ -9,13 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import corp.apps.salud.exception.ResourceNotFoundException;
@@ -56,7 +51,7 @@ public class EmployeeController {
 		return ResponseEntity.ok().body(employee);
 	}
 
-	@GetMapping("/deletebyid/{id}")
+	@DeleteMapping("/deletebyid/{id}")
 	public ResponseEntity<String> deleteById(@PathVariable long id) {
 		Optional<Employee> employeeOptional = repository.findById(id);
 
